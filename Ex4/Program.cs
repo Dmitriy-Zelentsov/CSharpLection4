5ead090d5ff2ec7940a308521349011b6003ba25
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-22 12:30:29 +0300

    add Ex4

---
+++ Ex4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Числа Фибоначи
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2) 
+
+int Fibonachi (int n)
+{
+    if(n == 1 || n == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fibonachi(n-1) + Fibonachi(n-2);
+    }
+}
+//Console.WriteLine(Fibonachi(5));
+
+for (int i = 1;  i < 50; i++)
+{
+    Console.WriteLine ($"f({i}) = {Fibonachi(i)}");
+}
+
